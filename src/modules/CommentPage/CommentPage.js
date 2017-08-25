6dcc7bb80f67e4a644d7dc8f6a492864aacb0196
Yogesh Devgun <yogeshdevgun@gmail.com>
2017-08-25 11:10:25 +0530

    Commenting UI and dummy Json added!

--- src/modules/CommentPage/CommentPage.js
+++ src/modules/CommentPage/CommentPage.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react'
 import {Link, withRouter} from 'react-router-dom';
 import ReactDOM from 'react-dom';
+import CommentStatic from '../../dummy/comments.json';
 
 export default class CommentPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
       commentInput: '',
-      postComments: []
+      postComments: [],
+      replyBox: false,
 
     }
 
@@ -15,17 +17,109 @@ export default class CommentPage extends Component {
   componentDidMount(){
     ReactDOM.findDOMNode(this.refs.comment).focus();
   }
+
+  replyBoxHandler(){
+    this.setState({replyBox:true})
+  }
   render() {
-    var comments = this.state.postComments.map((item, index) => {
+    var ShowReplyBox
+    if(this.state.replyBox==true){
+      ShowReplyBox =(
+        <input type="text" autoFocus/>
+      )
+    }
+    else{
+      ShowReplyBox=""
+    }
+    var replies = CommentStatic.map?CommentStatic.map((item,index)=>{
+        return (
+          <div key={index} className="Post Reply">
+            <div className="commentToCom_Container disp_inliFl">
+              <div className="comment_left">
+                <img alt="na" style={{height:'30px',width:'30px'}}></img>
+              </div>
+              <div className="commenttocomment_right ">
+                <div className="">
+                  <div className="comment_username">{item.reply[0].replyToReply[0].replyToReplyByUser.name}
+                  </div>
+                  <div className="comment_text">{item.reply[0].replyToReply[0].replyToReplyData}</div>
+                </div>
+                <div className="disp_inliFl comment_actions">
+                  <span className="act">Like</span>
+                  <span role="presentation" aria-hidden="true">&nbsp;·&nbsp;</span>
+                  <span className="pointer act" onClick={this.replyBoxHandler.bind(this)}>Reply</span>
+                  <span role="presentation" aria-hidden="true">&nbsp;·&nbsp;</span>
+                  <span className="act">Edit</span>
+                  <span role="presentation" aria-hidden="true">&nbsp;·&nbsp;</span>
+                  <span className="act">Delete</span>
+                </div>
+              </div>
+              </div>
+
+          </div>
+        )
+      }):''
+    var comments = CommentStatic.map((item, index) => {
       return(
-        <div>One</div>
+        <div>
+          <div className="comment_box displ_inlif">
+            <div className="comment_left">
+              <img className="user_img" alt="na"></img>
+            </div>
+            <div className="comment_right">
+              <div className="comment_username">{item.reply[0].replyByUser.name}</div>
+              <div  className="comment_text">{item.reply[0].replyData}</div>
+              <div className="comment_actions">
+                <span className="act">Like</span>
+                <span role="presentation" aria-hidden="true"> · </span>
+                  <span className="act">Reply</span>
+                  <span role="presentation" aria-hidden="true"> · </span>
+                <span className="act">Edit</span>
+                <span role="presentation" aria-hidden="true"> · </span>
+                <span className="act">Delete</span>
+              </div>
+            </div>
+          </div>
+          <div className="commentToComment_top_margin">
+             <div>
+               {item.reply[index].replyToReply.map((items, indexe) => {
+                   return (<div key={indexe} className="Post Reply">
+                     <div className="commentToCom_Container disp_inliFl">
+                       <div className="comment_left">
+                         <img alt="na" style={{height:'30px',width:'30px'}}></img>
+                       </div>
+                       <div className="commenttocomment_right ">
+                         <div className="">
+                           <div className="comment_username">{items.replyToReplyByUser.name}
+                           </div>
+                           <div className="comment_text">{items.replyToReplyData}</div>
+                         </div>
+                         <div className="disp_inliFl comment_actions">
+                           <span className="act">Like</span>
+                           <span role="presentation" aria-hidden="true">&nbsp;·&nbsp;</span>
+                           <span className="pointer act" onClick={this.replyBoxHandler.bind(this)}>Reply</span>
+                           <span role="presentation" aria-hidden="true">&nbsp;·&nbsp;</span>
+                           <span className="act">Edit</span>
+                           <span role="presentation" aria-hidden="true">&nbsp;·&nbsp;</span>
+                           <span className="act">Delete</span>
+                         </div>
+                       </div>
+                       </div>
+
+                   </div>)
+
+               })}
+             </div>
+                {ShowReplyBox}
+          </div>
+        </div>
       );
     })
     return (
-      <div>
+      <div className="comment_container">
         <form>
-          <textarea ref="comment" value={this.state.commentInput}></textarea>
-          <input type='submit' value='Post Comment'/>
+          <input type="text" placeholder="Write a comment..." ref="comment" value={this.state.commentInput}></input>
+          {/*<input type='submit' value='Post Comment'/>*/}
         </form>
         <div>
          {comments}
--- src/modules/PostPage/PostPage.js
+++ src/modules/PostPage/PostPage.js
@@ -8,11 +8,23 @@ export default class PostPage extends Component {
 
   render() {
     return (
-      <div>
-        <Link to="/">
+      <div className="post_container">
+        {/*<Link to="/">
           Register as new user
-        </Link><br/><br/>
-        <div>Time is precious. Waste it wisely.</div>
+        </Link><br/><br/>*/}
+        <div className="post_update disp_inliFl">
+          <div className="user_image">
+            <img alt="na"></img>
+          </div>
+          <div className="user_details">
+            <div className="post_username">Yogesh Devgun</div>
+            <div className="loc_time_container disp_inliFl">
+              <div>10 hrs</div>
+              <div className="post_location">Hyderabad</div>
+            </div>
+          </div>
+        </div>
+        <div className="post">Time is precious. Waste it wisely.</div>
         <CommentPage/>
       </div>
     )
